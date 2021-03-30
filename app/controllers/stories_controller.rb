a0d6a4aaf0e19dfb7f9f3fae834e8e012ab2c14c
Carlos E. La Torre <amautacelt@gmail.com>
2021-03-30 14:45:02 -0400

    added user functionality

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -8,6 +8,25 @@ class StoriesController < ApplicationController
         @story = Story.find(params[:id])
     end
 
+    def new
+        @story = Story.new
+    end
 
+    def create
+        @story = Story.create(story_params)
+        @current_user
+        if @story.valid?
+            redirect_to story_path(@story)
+        else
+            flash[:errors] = @story.errors.full_messages
+            redirect_to new_story_path
+        end
+    end
+
+    private
+
+    def story_params
+        params.require(:story).permit(:title, :content, :user_id)
+    end
 
 end
--- app/models/story.rb
+++ app/models/story.rb
@@ -1,4 +1,9 @@
 class Story < ApplicationRecord
     belongs_to :user
     has_many :contributions
+
+    validates :title, presence: true
+    validates :content, presence: true
+    validates :content, length: {maximum: 200}
+
 end
