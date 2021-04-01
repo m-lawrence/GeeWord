ee489ffaf2b81752859765e7f13ce47d69b9ed7d
Carlos E. La Torre <amautacelt@gmail.com>
2021-04-01 17:37:08 -0400

    color schema

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,13 +12,23 @@ class User < ApplicationRecord
     has_secure_password
 
     def self.most_stories
-        # story_count = User.all.each { |user| user.stories.count }
-        # user_story_counts = story_count.sort.reverse
         story_count = 0
         most_prolific = []
         User.all.each do |user|
         if user.stories.count > story_count
             story_count = user.stories.count
+            most_prolific << user
+        end
+        end
+        most_prolific.last.username
+    end
+
+    def self.most_contributions
+        contribution_count = 0
+        most_prolific = []
+        User.all.each do |user|
+        if user.contributions.count > contribution_count
+            contribrution_count = user.contributions.count
             most_prolific << user
         end
         end
